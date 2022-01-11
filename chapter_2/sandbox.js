21177b2bb29a5eb4272fc2525fbb47fae8c2bdb6
Mark LaPoint <marklapoint@gmail.com>
2022-01-11 13:40:36 -0700

    fixed
    fixed directory

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,12 @@
+let score = "100";
+
+//score + Number(score);
+
+//console.log(score + 1);
+//console.log(typeof score);
+
+//let result = Number("hello");
+//let result = String("50");
+let result = Boolean("");
+
+console.log(result, typeof result);
