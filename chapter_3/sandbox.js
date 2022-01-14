3615c8009aafe1c948f424bc2a947514298433e8
Mark LaPoint <marklapoint@gmail.com>
2022-01-14 11:44:21 -0700

    further advancement

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -14,10 +14,58 @@
 // console.log(html);
 //}
 
-const names = ["shaun", "mario", "luigi"];
+//const names = ["john", "ringo", "george", 'paul'];
 
-let i = 0;
-while (i < names.length) {
-  console.log(names[i]);
-  i++;
+//let i = 0;
+//while (i < names.length) {
+//  console.log(names[i]);
+//  i++;
+//}
+
+
+//do while loops
+
+//const age = 25;
+
+//if(age > 20){
+//  console.log('if you are over 20 years old');
+//}
+
+//const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+//if(ninjas.length > 2){
+//  console.log("that's a lot of ninjas");
+//}
+
+//else & else if statements
+
+//const password = 'pass';
+
+//if(password.length >= 12){
+//  console.log('that password is mighty strong')
+//} else if(password.length >= 8) {
+//  console.log('that password is long enough!');
+//} else {
+//  console.log('password is not long enough');
+//}
+
+
+//logical operators - OR || and AND &&
+
+//const password = 'pa@rd';
+
+//if(password.length >= 12 && password.includes('@')){
+//  console.log('that password is mighty strong');
+//} else if(password.length >= 8 || password.includes('@') && password.length >= 5) {
+//  console.log('that password is strong enough!');
+//} else {
+//  console.log('password is not long enough');
+//}
+
+//logical NOT (!)
+
+let user = false;
+
+if(user){
+
 }
